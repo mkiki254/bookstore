24f1227f85981c3b96c5fe63482b2f3cf642c7c3
mkiki254 <kariukicharles440@gmail.com>
2025-01-23 11:54:35 +0300

    Learned about user stories and looking for test coverage

--- bookstore_test.go
+++ bookstore_test.go
@@ -5,21 +5,44 @@ import (
 	"testing"
 )
 
-func TestBook(t *testing.T){
+func TestBook(t *testing.T) {
 	t.Parallel()
 	_ = bookstore.Book{
-		Title: "Spark Joy",
+		Title:  "Spark Joy",
 		Author: "Marie Kondo",
 		Copies: 2,
 	}
 }
 
-func TestBuy(t *Testing.T){
+func TestBuy(t *testing.T) {
 	t.Parallel()
 	b := bookstore.Book{
-		Title: "Nicholas Chuckleby",
-		Author: "Charles Dickens",
-		Copies: 8,
+		Title:  "Kidagaa Kimemwozea",
+		Author: "Ken Walibora",
+		Copies: 3,
 	}
-	tc
+
+	want := 2
+	result, err := bookstore.Buy(b)
+	got := result.Copies
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Errorf("Original copies were %d, Expected remaining copies to be %d, but got remaining copies as %d", b.Copies, want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Error("want error buying from zero copies, got nil")
+	}
 }
